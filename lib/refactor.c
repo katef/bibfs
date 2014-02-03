bf5a70c2f8eeada4ab4607bc088f14dca56dabc5
Kate F <kate@elide.org>
2014-02-03 15:54:11 +0000

    Filename normalisation to strip the `:file.pdf:PDF` metadata.

--- lib/refactor.c
+++ lib/refactor.c
@@ -5,6 +5,22 @@
 #include <bib/bib.h>
 #include <bib/refactor.h>
 
+static void
+normalise_file(const struct bib_field *f)
+{
+	struct bib_value *v;
+
+	assert(f != NULL);
+
+	for (v = f->value; v != NULL; v = v->next) {
+		/* ":/path/to/file.pdf:PDF" -> "/path/to/file.pdf" */
+		if (v->text[0] == ':') {
+			v->text++;
+			v->text[strcspn(v->text, ":")] = '\0';
+		}
+	}
+}
+
 static int
 refactor_entry(struct bib_entry *e)
 {
@@ -16,9 +32,12 @@ refactor_entry(struct bib_entry *e)
 			if (-1 == bib_split(p, ";")) {
 				return -1;
 			}
-		}
 
-		bib_merge(p);
+			bib_merge(p);
+			normalise_file(p);
+		} else {
+			bib_merge(p);
+		}
 	}
 
 	/* TODO: join values which shouldn't be multiple fields */
