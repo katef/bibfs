47cc203d35b48b915a775fe1b00cf20c91c75a68
Kate F <kate@elide.org>
2014-02-18 15:19:40 +0000

    Elide excess cleaning. Although I am not exactly sure how this can occur.

--- mk/clean.mk
+++ mk/clean.mk
@@ -4,8 +4,12 @@ RMDIR  ?= rm -r
 
 .for dir in ${DIR}
 clean::
+.if !empty(CLEAN:M${dir}/*:M*.mk)
 	${REMOVE} ${CLEAN:M${dir}/*:M*.mk}
+.endif
+.if !empty(CLEAN:M${dir}/*:N*.mk)
 	${REMOVE} ${CLEAN:M${dir}/*:N*.mk}
+.endif
 .endfor
 
 .for dir in ${DIR} ${BUILD}
