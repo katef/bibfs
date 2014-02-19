7baea4ec3e99f92704c8b4a153f0ee1f2e911096
Kate F <kate@elide.org>
2014-02-19 19:01:32 +0000

    A POSIX rwlock wrapper, for caller convenience.

---
+++ fuse/rwlock.c
@@ -0,0 +1,117 @@
+#define _XOPEN_SOURCE 500
+
+/*
+ * Note that errors are considered fatal here, since they may only occur from
+ * design flaws, hence sit in the same role as assertions.
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include <pthread.h>
+
+#include "rwlock.h"
+
+struct rwlock {
+	pthread_rwlock_t rwlock;
+};
+
+static void
+destroyattr(pthread_rwlockattr_t *attr)
+{
+	int r;
+
+	assert(attr != NULL);
+
+	r = pthread_rwlockattr_destroy(attr);
+	assert(0 == r);
+
+	(void) r;
+}
+
+struct rwlock *
+rwlock_create(void)
+{
+	pthread_rwlockattr_t attr;
+	struct rwlock *rw;
+	int r;
+
+	rw = malloc(sizeof *rw);
+	if (rw == NULL) {
+		return NULL;
+	}
+
+	r = pthread_rwlockattr_init(&attr);
+	if (0 != r) {
+		return NULL;
+	}
+
+	r = pthread_rwlock_init(&rw->rwlock, &attr);
+	if (0 != r) {
+		goto error;
+	}
+
+	destroyattr(&attr);
+
+	return rw;
+
+error:
+
+	destroyattr(&attr);
+
+	return NULL;
+}
+
+void
+rwlock_destroy(struct rwlock *rw)
+{
+	int r;
+
+	assert(rw != NULL);
+
+	r = pthread_rwlock_destroy(&rw->rwlock);
+	assert(0 == r);
+
+	(void) r;
+
+	free(rw);
+}
+
+void
+rwlock_lock(struct rwlock *rw, enum rwlock_type type)
+{
+	int r;
+
+	assert(rw != NULL);
+
+	switch (type) {
+	case RWLOCK_READ:  r = pthread_rwlock_rdlock(&rw->rwlock); break;
+	case RWLOCK_WRITE: r = pthread_rwlock_wrlock(&rw->rwlock); break;
+
+	default:
+		errno = EINVAL;
+		abort();
+	}
+
+	if (0 != r) {
+		perror("pthread_rwlock_wrlock");
+		abort();
+	}
+}
+
+void
+rwlock_unlock(struct rwlock *rw)
+{
+	int r;
+
+	assert(rw != NULL);
+
+	r = pthread_rwlock_unlock(&rw->rwlock);
+	if (0 != r) {
+		perror("pthread_rwlock_unlock");
+		abort();
+	}
+}
+
---
+++ fuse/rwlock.h
@@ -0,0 +1,30 @@
+#ifndef BIBFS_FUSE_RWLOCK_H
+#define BIBFS_FUSE_RWLOCK_H
+
+/*
+ * Non-recursive read/write locks. (Attempting to lock a locked lock is illegal)
+ *
+ * Recursive locks are intentionally not implemented for various reasons.
+ * In particular, although convenient, they are often a symptom of poor design.
+ * Here we attempt to keep to a minimal set of features to aid portability.
+ */
+
+enum rwlock_type {
+	RWLOCK_READ,
+	RWLOCK_WRITE
+};
+
+struct rwlock *
+rwlock_create(void);
+
+void
+rwlock_destroy(struct rwlock *);
+
+void
+rwlock_lock(struct rwlock *, enum rwlock_type);
+
+void
+rwlock_unlock(struct rwlock *);
+
+#endif
+
