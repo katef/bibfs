fcb83fd93a757cab1935939922d9ef4c4e727d15
Kate F <kate@elide.org>
2015-10-24 17:11:53 +0000

    Cheesy TeX escapes.

---
+++ include/bib/tex.h
@@ -0,0 +1,8 @@
+#ifndef BIB_TEX_H
+#define BIB_TEX_H
+
+char *
+tex_escape(char *dst, const char *src, int normalisecase);
+
+#endif
+
---
+++ lib/tex.c
@@ -0,0 +1,97 @@
+#include <assert.h>
+#include <string.h>
+#include <ctype.h>
+
+#include <bib/bib.h>
+#include <bib/tex.h>
+
+char *
+tex_escape(char *dst, const char *src, int normalisecase)
+{
+	const char *p;
+	char *q;
+	size_t i;
+	int depth;
+
+	static const struct {
+		const char *from;
+		const char *to;
+	} a[] = {
+		{ "---", "\xE2\x80\x94" },
+		{ "--",  "\xE2\x80\x93" },
+		{ "``",  "\xE2\x80\x9C" },
+		{ "''",  "\xE2\x80\x9D" },
+		{ "`",   "\xE2\x80\x98" },
+		{ "'",   "\xE2\x80\x99" },
+		{ "~",   "\xC2\xA0"     },
+
+/* TODO: flag for {"} vs \"
+		{ "\\\\", "\\" },
+		{ "\\'",  "'"  },
+		{ "\\\"", "\"" },
+*/
+		{ "\\$",  "$"  },
+		{ "\\{",  "{"  },
+		{ "\\}",  "}"  },
+
+		/* TODO: more */
+		{ "\\'e",  "\xC3\xA9" },
+		{ "\\\"e", "\xC3\xAB" },
+		{ "\\\"u", "\xC3\xBC" },
+	};
+
+	assert(src != NULL);
+	assert(dst != NULL);
+
+	depth = 0;
+
+	p = src;
+	q = dst;
+
+	while (*p != '\0') {
+		if (*p == '{') {
+			depth++;
+			p++;
+			continue;
+		}
+
+		if (*p == '}') {
+			depth--;
+			p++;
+			continue;
+		}
+
+		/* TODO: state for $$ math mode */
+
+		for (i = 0; i < sizeof a / sizeof *a; i++) {
+			if (0 == strncmp(p, a[i].from, strlen(a[i].from))) {
+				strcpy(q, a[i].to);
+				p += strlen(a[i].from);
+				q += strlen(a[i].to);
+				break;
+			}
+		}
+
+		if (i == sizeof a / sizeof *a) {
+			if (normalisecase) {
+				if (p == src) {
+					*q = toupper(*p);
+				} else if (depth == 0) {
+					*q = tolower(*p);
+				} else {
+					*q = *p;
+				}
+			} else {
+				*q = *p;
+			}
+
+			p++;
+			q++;
+		}
+	}
+
+	*q = '\0';
+
+	return dst;
+}
+
